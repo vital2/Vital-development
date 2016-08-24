f54b47a01659b290347b5fdc771c0d4635403be3
Richie Johnson <richie@Richies-MBP.home>
2016-08-23 23:21:34 -0400

    reads folder from config file

--- vital_site/xen_api/xen_api.py
+++ vital_site/xen_api/xen_api.py
@@ -1,5 +1,11 @@
 from subprocess import Popen, PIPE
 
+import ConfigParser
+
+
+config = ConfigParser.ConfigParser()
+config.read("/home/vlab/config.ini")
+
 
 class XenAPI:
 
@@ -8,7 +14,7 @@ class XenAPI:
 
     def start_vm(self, vm_name):
         # subprocess.call('xl create '+vm_name+".conf", shell=True)
-        cmd = 'xl create '+vm_name+'.conf'
+        cmd = 'xl create '+config.get("VMConfig", "VM_CONF_LOCATION")+'/'+vm_name+'.conf'
         p = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
         out, err = p.communicate()
         print "Return code: ", p.returncode
