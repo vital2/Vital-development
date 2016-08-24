c5441cc4bc3c21e12a1bd5ab8c39fc81ecc7b434
Richie Johnson <richie@Richies-MBP.home>
2016-08-24 16:52:05 -0400

    listing vms

--- vital_site/xen_api/xen_api.py
+++ vital_site/xen_api/xen_api.py
@@ -8,27 +8,50 @@ config.read("/home/vlab/config.ini")
 
 
 class XenAPI:
+    """ Provides api to xen operations """
 
     def __init__(self):
         pass
 
     def start_vm(self, vm_name):
-        # subprocess.call('xl create '+vm_name+".conf", shell=True)
-        cmd = 'xl create '+config.get("VMConfig", "VM_CONF_LOCATION")+'/'+vm_name+'.conf'
+        VirtualMachine(vm_name).start()
+
+    def list_vms(self):
+        cmd = 'xl list'
         p = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
         out, err = p.communicate()
-        print "Return code: ", p.returncode
-        print '>'*80
-        print out.rstrip()
-        print '>' * 80
-        print err.rstrip()
         if not p.returncode == 0:
             raise Exception('ERROR : cannot start the vm. \n Reason : %s' % err.rstrip())
 
-    def check_if_port_is_used(self):
-        pass
+        for line in p.stdout.readlines():
+            val = line.strip().split("\t")
+            print val
+
+        # print "Return code: ", p.returncode
+        # print '>'*80
+        # print out.rstrip()
+        # print '>' * 80
+        # print err.rstrip()
 
     def server_stats(self):
         pass
 
-XenAPI().start_vm("bt5-qemu73")
+
+class VirtualMachine:
+    """ References virtual machines which Xen services """
+
+    def __init__(self, name):
+        self.name = name
+
+    def start(self):
+        cmd = 'xl create ' + config.get("VMConfig", "VM_CONF_LOCATION") + '/' + vm_name + '.conf'
+        p = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
+        out, err = p.communicate()
+        if not p.returncode == 0:
+            raise Exception('ERROR : cannot start the vm. \n Reason : %s' % err.rstrip())
+        else:
+            return self
+
+
+# XenAPI().start_vm("bt5-qemu73")
+XenAPI().list_vms()
