c2860718baa27d2edd9a3d10c8ef3fcd460f1baa
Richie Johnson <richie@Richies-MacBook-Pro.local>
2016-08-25 01:53:23 -0400

    register vm

--- vital_site/xen_api/TestClient.py
+++ vital_site/xen_api/TestClient.py
@@ -13,17 +13,17 @@ print "Registering new VM..."
 proxy.xenapi.register_vm('richiedjohnson@yahoo.co.in', 'Test123!', 'GY12345_bt5', '3', '1')
 
 print "Stopping vm if exists..."
-proxy.xenapi.stop_vm('richiedjohnson@yahoo.co.in', 'Test123!', 'GY12345_bt5')
+proxy.xenapi.stop_vm('richiedjohnson@yahoo.co.in', 'Test123!', 'GY12345_bt5', '3', '1')
 
 print "Starting VM..."
-vm = proxy.xenapi.start_vm('richiedjohnson@yahoo.co.in', 'Test123!', 'GY12345_bt5')
+vm = proxy.xenapi.start_vm('richiedjohnson@yahoo.co.in', 'Test123!', 'GY12345_bt5', '3', '1')
 pprint(vars(vm))
 
 print "Listing all VMs..."
 print proxy.xenapi.list_all_vms('richiedjohnson@yahoo.co.in', 'Test123!')
 
 print "Stopping created VM..."
-proxy.xenapi.stop_vm('richiedjohnson@yahoo.co.in', 'Test123!', 'GY12345_bt5')
+proxy.xenapi.stop_vm('richiedjohnson@yahoo.co.in', 'Test123!', 'GY12345_bt5', '3', '1')
 
 print "Listing all VMs..."
 print proxy.xenapi.list_all_vms('richiedjohnson@yahoo.co.in', 'Test123!')
