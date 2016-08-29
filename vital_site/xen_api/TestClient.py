766cf9b08968ac480c68c4913d1fda985a29fe13
Richie Johnson <richie@Richies-MBP.home>
2016-08-28 20:02:38 -0400

    clean ups

--- vital_site/xen_api/TestClient.py
+++ vital_site/xen_api/TestClient.py
@@ -10,23 +10,23 @@ print "Listing specific VM..."
 pprint(proxy.xenapi.list_vm('rdj259@nyu.edu', 'pbkdf2_sha256$24000$WK1SdV4Vx29M$kwlqioZRM2LGlbBo0nRGyFUoZQnEke84jKaOzIMaZ64=', 'bt5-qemu14'))
 
 print "Registering new VM..."
-proxy.xenapi.register_vm('rdj259@nyu.edu', 'pbkdf2_sha256$24000$WK1SdV4Vx29M$kwlqioZRM2LGlbBo0nRGyFUoZQnEke84jKaOzIMaZ64=', '1_3_1', "3_1")  # <<studentid_courseid_vmid>>
+proxy.xenapi.register_vm('rdj259@nyu.edu', 'pbkdf2_sha256$24000$WK1SdV4Vx29M$kwlqioZRM2LGlbBo0nRGyFUoZQnEke84jKaOzIMaZ64=', '1_2_1', "2_1")  # <<studentid_courseid_vmid>>
 
 print "Stopping vm if exists..."
-proxy.xenapi.stop_vm('rdj259@nyu.edu', 'pbkdf2_sha256$24000$WK1SdV4Vx29M$kwlqioZRM2LGlbBo0nRGyFUoZQnEke84jKaOzIMaZ64=', '1_3_1')
+proxy.xenapi.stop_vm('rdj259@nyu.edu', 'pbkdf2_sha256$24000$WK1SdV4Vx29M$kwlqioZRM2LGlbBo0nRGyFUoZQnEke84jKaOzIMaZ64=', '1_2_1')
 
 print "Starting VM..."
-vm = proxy.xenapi.start_vm('rdj259@nyu.edu', 'pbkdf2_sha256$24000$WK1SdV4Vx29M$kwlqioZRM2LGlbBo0nRGyFUoZQnEke84jKaOzIMaZ64=', '1_3_1')
+vm = proxy.xenapi.start_vm('rdj259@nyu.edu', 'pbkdf2_sha256$24000$WK1SdV4Vx29M$kwlqioZRM2LGlbBo0nRGyFUoZQnEke84jKaOzIMaZ64=', '1_2_1')
 pprint(vm)
 
 print "Listing all VMs..."
 print proxy.xenapi.list_all_vms('rdj259@nyu.edu', 'pbkdf2_sha256$24000$WK1SdV4Vx29M$kwlqioZRM2LGlbBo0nRGyFUoZQnEke84jKaOzIMaZ64=')
 
 #print "Stopping created VM..."
-#proxy.xenapi.stop_vm('rdj259@nyu.edu', 'pbkdf2_sha256$24000$WK1SdV4Vx29M$kwlqioZRM2LGlbBo0nRGyFUoZQnEke84jKaOzIMaZ64=', '1_3_1')
+#proxy.xenapi.stop_vm('rdj259@nyu.edu', 'pbkdf2_sha256$24000$WK1SdV4Vx29M$kwlqioZRM2LGlbBo0nRGyFUoZQnEke84jKaOzIMaZ64=', '1_2_1')
 
 #print "Unregistering VM"
-#proxy.xenapi.unregister_vm('rdj259@nyu.edu', 'pbkdf2_sha256$24000$WK1SdV4Vx29M$kwlqioZRM2LGlbBo0nRGyFUoZQnEke84jKaOzIMaZ64=', '1_3_1')
+#proxy.xenapi.unregister_vm('rdj259@nyu.edu', 'pbkdf2_sha256$24000$WK1SdV4Vx29M$kwlqioZRM2LGlbBo0nRGyFUoZQnEke84jKaOzIMaZ64=', '1_2_1')
 
 #print "Listing all VMs..."
 #print proxy.xenapi.list_all_vms('rdj259@nyu.edu', 'pbkdf2_sha256$24000$WK1SdV4Vx29M$kwlqioZRM2LGlbBo0nRGyFUoZQnEke84jKaOzIMaZ64=')
