f181553c435a27676ac91c19390858dea2c35668
ab6466 <ab6466@nyu.edu>
2016-10-13 15:23:05 -0400

    Update on_web_server_startup.sh

--- vital_site/scripts/on_web_server_startup.sh
+++ vital_site/scripts/on_web_server_startup.sh
@@ -7,10 +7,20 @@ mount -t glusterfs gusterfs1-dev:volume1 /mnt/vlab-datastore
 # TODO Needs to be populated automatically from database
 # TODO Find a better location for nat-start script
 # TODO replace with course_startup script
-/home/vlab_scp/vmnet_conf/vlab-natdhcp/nat-start.sh 12
-/home/vlab_scp/vmnet_conf/vlab-natdhcp/nat-start.sh 14
+
+#Arjun - edit
+for var in "$@"
+do
+    /home/vlab_scp/vmnet_conf/vlab-natdhcp/nat-start.sh var
+done
+#edit - done
+
+#/home/vlab_scp/vmnet_conf/vlab-natdhcp/nat-start.sh 12
+#/home/vlab_scp/vmnet_conf/vlab-natdhcp/nat-start.sh 14
 
 # enables SFTP access to the SFTP server from xen vms
+# TODO change the hardcorded server IP
+
 SERVER_IP="128.238.66.35"
 iptables -I FORWARD 1 -p icmp --icmp-type 8 -s 0/0 -d $SERVER_IP -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
 iptables -I FORWARD 1 -p icmp --icmp-type 0 -s $SERVER_IP -d 0/0 -m state --state ESTABLISHED,RELATED -j ACCEPT
