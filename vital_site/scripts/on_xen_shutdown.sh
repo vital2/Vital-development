421015a051a14f0ded3053111c34bf4245a399f6
ab6466 <ab6466@nyu.edu>
2016-10-13 15:44:22 -0400

    Update on_xen_shutdown.sh

--- vital_site/scripts/on_xen_shutdown.sh
+++ vital_site/scripts/on_xen_shutdown.sh
@@ -1,2 +1,12 @@
 # unmount gluster
 # reset iptables
+
+#Arjun - edit
+umount /mnt/vlab-datastore
+echo "Log created from on_xen_shutdown.sh "
+echo " Gluster unmounted "
+
+sudo iptables -F
+echo "Iptables rules flushed"
+
+#edit - done
