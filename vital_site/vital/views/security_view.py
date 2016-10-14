7270f9f42d9a3ca74bf25beac9e1e9ccdc529c5b
Richie Johnson <richiedjohnson@yahoo.co.in>
2016-10-14 12:56:34 -0400

    updated activation page to directly go to home screen

--- vital_site/vital/views/security_view.py
+++ vital_site/vital/views/security_view.py
@@ -74,6 +74,7 @@ def activate(request):
                         user.is_active = True
                         user.activation_code = None
                         user.save()
+                        update_session_auth_hash(request, user)
                         logger.debug('activated..'+user.email)
                         form = Authentication_Form()
                         #return render(request, 'vital/login.html', {'form': form })
