7b8457966eb30ba9e807c9c3670e52ffec84c565
Richie Johnson <richiedjohnson@yahoo.co.in>
2016-11-27 16:19:02 -0500

    kill all user vms on logout

--- vital_site/vital/views/student_view.py
+++ vital_site/vital/views/student_view.py
@@ -204,17 +204,17 @@ def fix_mccoy_user_vms(request):
         for value in registered:
             logger.debug('fixing ' + str(value.user_id) + '_3_2')
             available_config = Available_Config.objects.filter(category='MAC_ADDR').order_by('id').last()
-            locked_conf = Available_Config.objects.select_for_update().filter(id=available_config.id)
-            if locked_conf is not None and len(locked_conf) > 0:
-                val = locked_conf[0].value
-                locked_conf.delete()
-
-                vif = '\'mac=' + val + ', bridge=Net-CS6823\''
-                logger.debug('Updating with vif:' + vif + ' for user ' + value.user_id)
-                user_net_config = User_Network_Configuration()
-                user_net_config.bridge_name = 'Net-CS6823'
-                user_net_config.user_id = value.user_id
-                user_net_config.mac_id = val
-                user_net_config.vm = vm
-                user_net_config.save()
-                logger.debug('Updating user ' + value.user_id)
+            with transaction.atomic():
+                locked_conf = Available_Config.objects.select_for_update().filter(id=available_config.id)
+                if locked_conf is not None and len(locked_conf) > 0:
+                    val = locked_conf[0].value
+                    locked_conf.delete()
+                    vif = '\'mac=' + val + ', bridge=Net-CS6823\''
+                    logger.debug('Updating with vif:' + vif + ' for user ' + value.user_id)
+                    user_net_config = User_Network_Configuration()
+                    user_net_config.bridge_name = 'Net-CS6823'
+                    user_net_config.user_id = value.user_id
+                    user_net_config.mac_id = val
+                    user_net_config.vm = vm
+                    user_net_config.save()
+                    logger.debug('Updating user ' + value.user_id)
