4e8b9263abb5ed0b058465b47bc8d262012443c2
Richie Johnson <richiedjohnson@yahoo.co.in>
2016-11-27 16:15:21 -0500

    kill all user vms on logout

--- vital_site/vital/views/student_view.py
+++ vital_site/vital/views/student_view.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.decorators import login_required
-from ..models import Course, Registered_Course, Virtual_Machine, User_VM_Config, Available_Config
+from ..models import Course, Registered_Course, Virtual_Machine, User_VM_Config, Available_Config, \
+    User_Network_Configuration
 from ..forms import Course_Registration_Form
 from ..utils import audit, XenClient
 import logging
@@ -199,5 +200,21 @@ def fix_mccoy_user_vms(request):
     if request.user.id == 2:
         course = Course.objects.get(id=3)
         registered = Registered_Course.objects.filter(course=course)
-        for val in registered:
-            logger.debug('fixing ' + str(val.user_id) + '_3_2')
+        vm = Virtual_Machine.objects.get(id=2)
+        for value in registered:
+            logger.debug('fixing ' + str(value.user_id) + '_3_2')
+            available_config = Available_Config.objects.filter(category='MAC_ADDR').order_by('id').last()
+            locked_conf = Available_Config.objects.select_for_update().filter(id=available_config.id)
+            if locked_conf is not None and len(locked_conf) > 0:
+                val = locked_conf[0].value
+                locked_conf.delete()
+
+                vif = '\'mac=' + val + ', bridge=Net-CS6823\''
+                logger.debug('Updating with vif:' + vif + ' for user ' + value.user_id)
+                user_net_config = User_Network_Configuration()
+                user_net_config.bridge_name = 'Net-CS6823'
+                user_net_config.user_id = value.user_id
+                user_net_config.mac_id = val
+                user_net_config.vm = vm
+                user_net_config.save()
+                logger.debug('Updating user ' + value.user_id)
