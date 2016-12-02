1a18622f26b4f527c3a347540a2fba1882356d05
Richie Johnson <richiedjohnson@yahoo.co.in>
2016-12-02 13:02:59 -0500

    update to kill zombies

--- vital_site/xen_api/TestClient.py
+++ vital_site/xen_api/TestClient.py
@@ -1,32 +1,32 @@
 import xmlrpclib
 from pprint import pprint
 
-proxy = xmlrpclib.ServerProxy('http://128.238.77.10:8000')
+proxy = xmlrpclib.ServerProxy('http://192.168.35.11:8000')
 
 print "Listing all VMs..."
-print proxy.xenapi.list_all_vms('rdj259@nyu.edu', 'pbkdf2_sha256$24000$pyleadufja9u$0UM+faiyErEcJj8XFikLys9Cnu7rpyFPS3Hqw0G+eWY=')
+print proxy.xenapi.list_all_vms('rdj259@nyu.edu', 'pbkdf2_sha256$24000$rvI8ja8A8EVx$vvBZNpvNr72fBsmcNOJqIsKqrf9uyUM5PoLuivxbcoo=')
 
-print "Listing specific VM..."
-pprint(proxy.xenapi.list_vm('rdj259@nyu.edu', 'pbkdf2_sha256$24000$pyleadufja9u$0UM+faiyErEcJj8XFikLys9Cnu7rpyFPS3Hqw0G+eWY=', 'Domain-0'))
+#print "Registering new VM..."
+#proxy.xenapi.register_vm('rdj259@nyu.edu', 'pbkdf2_sha256$24000$rvI8ja8A8EVx$vvBZNpvNr72fBsmcNOJqIsKqrf9uyUM5PoLuivxbcoo=', '1_2_1', "2_1")  # <<studentid_courseid_vmid>>
 
-print "Registering new VM..."
-proxy.xenapi.register_vm('rdj259@nyu.edu', 'pbkdf2_sha256$24000$pyleadufja9u$0UM+faiyErEcJj8XFikLys9Cnu7rpyFPS3Hqw0G+eWY=', '1_2_1', "2_1")  # <<studentid_courseid_vmid>>
-
-print "Stopping vm if exists..."
-proxy.xenapi.stop_vm('rdj259@nyu.edu', 'pbkdf2_sha256$24000$pyleadufja9u$0UM+faiyErEcJj8XFikLys9Cnu7rpyFPS3Hqw0G+eWY=', '1_2_1')
+#print "Stopping vm if exists..."
+#proxy.xenapi.stop_vm('rdj259@nyu.edu', 'pbkdf2_sha256$24000$rvI8ja8A8EVx$vvBZNpvNr72fBsmcNOJqIsKqrf9uyUM5PoLuivxbcoo=', '1_2_1')
 
 print "Starting VM..."
-vm = proxy.xenapi.start_vm('rdj259@nyu.edu', 'pbkdf2_sha256$24000$pyleadufja9u$0UM+faiyErEcJj8XFikLys9Cnu7rpyFPS3Hqw0G+eWY=', '1_2_1')
+vm = proxy.xenapi.start_vm('rdj259@nyu.edu', 'pbkdf2_sha256$24000$rvI8ja8A8EVx$vvBZNpvNr72fBsmcNOJqIsKqrf9uyUM5PoLuivxbcoo=', '2_3_2')
 pprint(vm)
 
-print "Listing all VMs..."
-print proxy.xenapi.list_all_vms('rdj259@nyu.edu', 'pbkdf2_sha256$24000$pyleadufja9u$0UM+faiyErEcJj8XFikLys9Cnu7rpyFPS3Hqw0G+eWY=')
+print "Listing specific VM..."
+pprint(proxy.xenapi.list_vm('rdj259@nyu.edu', 'pbkdf2_sha256$24000$rvI8ja8A8EVx$vvBZNpvNr72fBsmcNOJqIsKqrf9uyUM5PoLuivxbcoo=', '2_3_2'))
 
 print "Stopping created VM..."
-proxy.xenapi.stop_vm('rdj259@nyu.edu', 'pbkdf2_sha256$24000$pyleadufja9u$0UM+faiyErEcJj8XFikLys9Cnu7rpyFPS3Hqw0G+eWY=', '1_2_1')
-
-print "Unregistering VM"
-proxy.xenapi.unregister_vm('rdj259@nyu.edu', 'pbkdf2_sha256$24000$pyleadufja9u$0UM+faiyErEcJj8XFikLys9Cnu7rpyFPS3Hqw0G+eWY=', '1_2_1')
+proxy.xenapi.stop_vm('rdj259@nyu.edu', 'pbkdf2_sha256$24000$rvI8ja8A8EVx$vvBZNpvNr72fBsmcNOJqIsKqrf9uyUM5PoLuivxbcoo=', '2_3_2')
 
 print "Listing all VMs..."
-print proxy.xenapi.list_all_vms('rdj259@nyu.edu', 'pbkdf2_sha256$24000$pyleadufja9u$0UM+faiyErEcJj8XFikLys9Cnu7rpyFPS3Hqw0G+eWY=')
+print proxy.xenapi.list_all_vms('rdj259@nyu.edu', 'pbkdf2_sha256$24000$rvI8ja8A8EVx$vvBZNpvNr72fBsmcNOJqIsKqrf9uyUM5PoLuivxbcoo=')
+
+#print "Unregistering VM"
+#proxy.xenapi.unregister_vm('rdj259@nyu.edu', 'pbkdf2_sha256$24000$rvI8ja8A8EVx$vvBZNpvNr72fBsmcNOJqIsKqrf9uyUM5PoLuivxbcoo=', '1_2_1')
+
+#print "Listing all VMs..."
+#print proxy.xenapi.list_all_vms('rdj259@nyu.edu', 'pbkdf2_sha256$24000$rvI8ja8A8EVx$vvBZNpvNr72fBsmcNOJqIsKqrf9uyUM5PoLuivxbcoo=')
