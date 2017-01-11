b08c19a93aa261fc39a8b39bc8b37a056851ba69
Richie Johnson <richiedjohnson@yahoo.co.in>
2017-01-11 06:17:44 -0500

    adding on startup script to setup networking

--- vital_site/scripts/listen_xen_stats.sh
+++ vital_site/scripts/listen_xen_stats.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-workon vital
-nohup python /home/rdj259/vital2.0/source/virtual_lab/vital_site/manage.py StartServerStatsCollection &
+# this is called by upstart job on apache2 start - /etc/init/upstart/vital_listen_xen_stats.conf
+# workon vital - instead point to virtualenv python installation
+nohup /home/rdj259/.virtualenvs/vital/bin/python /home/rdj259/vital2.0/source/virtual_lab/vital_site/manage.py StartServerStatsCollection &
