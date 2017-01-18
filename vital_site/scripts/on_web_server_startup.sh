7259373d8131b22be20bd0f019a952e90f36a00f
Richie Johnson <richiedjohnson@yahoo.co.in>
2017-01-18 08:38:03 -0500

    fixing startup script

--- vital_site/scripts/on_web_server_startup.sh
+++ vital_site/scripts/on_web_server_startup.sh
@@ -7,7 +7,7 @@
 mount -t glusterfs gusterfs1-dev:volume1 /mnt/vlab-datastore
 
 # sets up required bridges and bonds for the courses on web server
-nets=$(psql -U postgres -d vital_db -t -c "SELECT c.id from vital_course c join vital_network_configuration n on c.id=n.course_id where c.status='ACTIVE'")
+nets=$(psql -U postgres -d vital_db -t -c "SELECT c.id from vital_course c join vital_network_configuration n on c.id=n.course_id where c.status='ACTIVE' and n.is_course_net=True")
 set -f
 array=(${nets// / })
 
