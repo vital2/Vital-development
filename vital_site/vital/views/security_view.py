4f29294ba69d34984d22b5ab077b1e42dccc194b
Richie Johnson <richiedjohnson@yahoo.co.in>
2017-01-23 17:33:03 -0500

    updated email text

--- vital_site/vital/views/security_view.py
+++ vital_site/vital/views/security_view.py
@@ -102,8 +102,8 @@ def activate(request):
                                   'Hi ' + user.first_name + ',\r\n\n Welcome to Vital. Your account has been activated. '
                                                             'Please follow instructions '
                                                             'from your instructor to access the course VMs. '
-                                                            '\r\nYour SFTP account is '+user.sftp_account+' and password'
-                                                            ' is the same as vital\r\n\nVital',
+                                                            '\r\n\nSFTP Host : 128.238.66.35 \n Your SFTP account is '+user.sftp_account+' and password'
+                                                            ' is the same as vital.  \r\n\nVital',
                                   'no-reply-vital@nyu.edu', [user.email], fail_silently=False)
                         logger.debug('activated..'+user.email)
                         form = Authentication_Form()
