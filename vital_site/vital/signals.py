60c0802a89746ab2739a745c16f05edadec7653f
richiedjohnson <richiedjohnson@yahoo.co.in>
2017-03-02 00:26:31 -0500

    removes session entry on logout

--- vital_site/vital/signals.py
+++ vital_site/vital/signals.py
@@ -3,7 +3,7 @@ from django.dispatch import receiver
 from django.conf import settings
 import logging
 from . import views
-
+from django.contrib.sessions.models import Session
 
 @receiver(user_logged_in)
 def sig_user_logged_in(sender, user, request, **kwargs):
@@ -14,8 +14,11 @@ def sig_user_logged_in(sender, user, request, **kwargs):
 @receiver(user_logged_out)
 def sig_user_logged_out(sender, user, request, **kwargs):
     logger = logging.getLogger(__name__)
-    logger.info("user logged out: %s at %s" % (user, request.META['REMOTE_ADDR']))
     views.stop_vms_during_logout(user)
+    session = Session.objects.get(session_key=request.session.session_key)
+    session.delete()
+    logger.info("user logged out: %s at %s" % (user, request.META['REMOTE_ADDR']))
+
     # do machine shut down and other cleanup activities here
 
 # this still would require a cron job to make sure that logged out on browser close can be handled - cron.py
