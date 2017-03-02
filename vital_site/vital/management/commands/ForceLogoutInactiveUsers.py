9f50d25e8e077a5ea4233e676dff2b92b0c6b31b
richiedjohnson <richiedjohnson@yahoo.co.in>
2017-03-02 01:23:07 -0500

    force logout inactive users

--- vital_site/vital/management/commands/ForceLogoutInactiveUsers.py
+++ vital_site/vital/management/commands/ForceLogoutInactiveUsers.py
@@ -2,6 +2,8 @@ from django.core.management.base import BaseCommand, CommandError
 from django.contrib.sessions.models import Session
 import logging
 from datetime import datetime
+from vital.models import VLAB_User
+from vital.views import stop_vms_during_logout
 
 logger = logging.getLogger(__name__)
 
@@ -11,8 +13,10 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         sessions = Session.objects.filter(expire_date__lt=datetime.now())
         for session in sessions:
-            user = session.get_decoded().get('_auth_user_id')
-            if user is not None:
-                logger.debug('session user_id:' +user)
-            else:
-                logger.debug('session user_id:Unknown')
+            user_id = session.get_decoded().get('_auth_user_id')
+            if user_id is not None:
+                logger.debug('session user_id:' +user_id)
+                user = VLAB_User.objects.get(id=user_id)
+                logger.debug("Force shutting down VMs for user : "+user.email)
+                stop_vms_during_logout(user)
+            session.delete()
