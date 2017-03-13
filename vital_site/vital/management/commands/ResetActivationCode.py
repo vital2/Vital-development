1720cdfe11e06369561f8725975ae5c0b4fa6788
Anusha Sharma <as10553@nyu.edu>
2017-03-12 22:07:14 -0400

    reset activation code command

--- vital_site/vital/management/commands/ResetActivationCode.py
+++ vital_site/vital/management/commands/ResetActivationCode.py
@@ -1,10 +1,34 @@
 from django.core.management.base import BaseCommand, CommandError
 import logging
+from django.core.mail import send_mail
+from vital.models import VLAB_User
+from random import randint
 
 logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
+    help = "reset activation code for a user"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '-e', '--email',
+            action='store',
+            dest='email',
+            help='specify user email',
+            required=True
+        )
 
     def handle(self, *args, **options):
-        pass
+        email = options['email']
+        user = VLAB_User.objects.filter(email)
+        user.activation_code = False
+        activation_code = randint(100000, 999999)
+        user.activation_code = activation_code
+        user.save()
+        send_mail('Activation code reset mail',
+                  'Hi ' + user.first_name + ',\r\n\n Please re-enter this activation code to '
+                                            'access your course VMs on vital. '
+                  + user.email + '&activation_code=' + str(activation_code)
+                  + '.\r\n\nVital', 'no-reply-vital@nyu.edu',
+                  [user.email], fail_silently=False)
