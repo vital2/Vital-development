e615c901f9627b98455b4551a50ca3e569172bba
Anusha Sharma <as10553@nyu.edu>
2017-03-14 23:31:43 -0400

    reset activation code support command

--- vital_site/vital/management/commands/ResetActivationCode.py
+++ vital_site/vital/management/commands/ResetActivationCode.py
@@ -22,13 +22,11 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         email = options['email']
         user = VLAB_User.objects.filter(email)
-        user.activation_code = False
+        user.is_active = False
         activation_code = randint(100000, 999999)
         user.activation_code = activation_code
         user.save()
         send_mail('Activation code reset mail',
-                  'Hi ' + user.first_name + ',\r\n\n Please re-enter this activation code to '
-                                            'access your course VMs on vital. '
-                  + user.email + '&activation_code=' + str(activation_code)
-                  + '.\r\n\nVital', 'no-reply-vital@nyu.edu',
-                  [user.email], fail_silently=False)
+                  'Hi ' + user.first_name + ',\r\n\n Please enter the activation code' + str(activation_code) +
+                                            ' to access your vital account.' +
+                  + '.\r\n\nVital', 'no-reply-vital@nyu.edu', [user.email], fail_silently=False)
