4ebe8091187c16858f7a5b4f78b057b20ca2fd1b
richiedjohnson <richiedjohnson@yahoo.co.in>
2017-03-16 16:41:22 -0400

    revert back to normal novnc

--- vital_site/vital/views/student_view.py
+++ vital_site/vital/views/student_view.py
@@ -47,21 +47,21 @@ def course_vms(request, course_id):
     logger.debug("in course vms")
     params = dict()
     virtual_machines = Virtual_Machine.objects.filter(course_id=course_id)
-    # server_name = config_ini.get('VITAL', 'SERVER_NAME')
+    server_name = config_ini.get('VITAL', 'SERVER_NAME')
     for vm in virtual_machines:
         user_vm_configs = vm.user_vm_config_set.filter(user_id=request.user.id)
         if user_vm_configs is not None and not len(user_vm_configs) == 0:
             vm.state = 'R'
-            # user_vm_configs = vm.user_vm_config_set.filter(user_id=request.user.id)
-            # for config in user_vm_configs:
-            #    if config.vm.id == vm.id:
-            #       vm.terminal_port = config.terminal_port
-            #       break
+            user_vm_configs = vm.user_vm_config_set.filter(user_id=request.user.id)
+            for config in user_vm_configs:
+                if config.vm.id == vm.id:
+                    vm.terminal_port = config.terminal_port
+                    break
         else:
             vm.state = 'S'
     params['virtual_machines'] = virtual_machines
     params['course_id'] = course_id
-    # params['server_name'] = server_name
+    params['server_name'] = server_name
 
     if not request.GET.get('message', '') == '':
         params['message'] = request.GET.get('message')
