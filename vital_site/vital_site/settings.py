2a8474f0d472aad6731cbe5622160b380562579b
Anusha Sharma <as10553@nyu.edu>
2017-07-17 16:11:35 -0400

    captcha refresh

--- vital_site/vital_site/settings.py
+++ vital_site/vital_site/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = '/home/rdj259/vital_static'
 
-CAPTCHA_IMAGE_SIZE = (120, 60)
+CAPTCHA_IMAGE_SIZE = (120, 50)
 CAPTCHA_FONT_SIZE = 32
 CAPTCHA_OUTPUT_FORMAT = u'%(image)s %(hidden_field)s <br> %(text_field)s'
 
