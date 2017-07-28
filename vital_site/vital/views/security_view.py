61ff6fdf6856f8a4e01009c0f2afc2a2d1998d8a
Anusha Sharma <as10553@nyu.edu>
2017-07-28 17:21:43 -0400

    redirect password reset to login

--- vital_site/vital/views/security_view.py
+++ vital_site/vital/views/security_view.py
@@ -221,7 +221,8 @@ def forgot_password(request):
             except VLAB_User.DoesNotExist:
                 logger.debug('Cannot find requested email -'+form.cleaned_data['email'])
             message = "Email with reset password instructions is on the way"
-            return render(request, 'vital/login.html', {'message': message})
+            form = Authentication_Form()
+            return render(request, 'vital/login.html', {'form': form,'message': message})
     else:
         form = Forgot_Password_Form()
     return render(request, 'vital/forgot_password.html', {'form': form, 'message': message})
