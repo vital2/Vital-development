e5ac06acad2c6d1d0fc0c87cff8324f44a410880
Anusha Sharma <as10553@nyu.edu>
2018-03-13 23:34:28 -0400

    index view

--- vital_site/authoring/views.py
+++ vital_site/authoring/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+import logging
+import ConfigParser
+from django.contrib.auth.decorators import login_required
+
+
+config_ini = ConfigParser.ConfigParser()
+config_ini.optionxform=str
+
+config_ini.read("/home/vital/config.ini")
+logger = logging.getLogger(__name__)
 
 # Create your views here.
+
+
+@login_required(login_url='/vital/login/')
+def index(request):
+    return render(request, 'vital/index.html')
+
