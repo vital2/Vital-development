a02e3e24d276c609556d7adb12ce839737ace302
Anusha Sharma <as10553@nyu.edu>
2018-04-02 22:19:56 -0400

    first view

--- vital_site/authoring/views.py
+++ vital_site/authoring/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from ..models import Course, Registered_Course, Virtual_Machine, User_VM_Config, Available_Config, \
+    User_Network_Configuration
+from ..forms import Course_Registration_Form
+from ..utils import audit, XenClient, get_notification_message
+import logging
+import ConfigParser
 
-# Create your views here.
+logger = logging.getLogger(__name__)
+config_ini = ConfigParser.ConfigParser()
+config_ini.optionxform = str
+
+# TODO change to common config file in shared location
+config_ini.read("/home/vital/config.ini")
+
+
+def index(request):
+    return render(request, '/authoring/index.html')
