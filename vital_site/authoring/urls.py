1be97c4f4a2d0222fae83dfb6689e52b732bdcd1
Anusha Sharma <as10553@nyu.edu>
2018-04-05 17:14:36 -0400

    added course create and destroy views

--- vital_site/authoring/urls.py
+++ vital_site/authoring/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^courses/home/$', views.course_home, name='course_home'),
     url(r'^courses/create/$', views.course_create, name='course_create'),
-    url(r'^courses/vms/$', views.course_vm_setup, name='course_vm_setup'),
+    url(r'^courses/vms/$', views.course_vm_setup, name='course_vms'),
     url(r'^courses/destroy/$', views.course_destroy, name='course_destroy'),
 ]
