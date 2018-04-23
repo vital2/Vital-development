3b1f0b17a46f361a43816dc8a07d0f493c7b6a06
Anusha Sharma <as10553@nyu.edu>
2018-04-22 23:19:56 -0400

    model choice field

--- vital_site/authoring/forms/create_form.py
+++ vital_site/authoring/forms/create_form.py
@@ -14,7 +14,7 @@ class CreateCourseForm(forms.Form):
 class CreateVmsForm(ModelForm):
     class Meta:
         model = Virtual_Machine
-        fields = ['course', 'name', 'type']
+        fields = ['name', 'type']
 
     def __init__(self, *args, **kwargs):
         super(CreateVmsForm, self).__init__(*args, **kwargs)
