909f806c972481075b882787b07c8e0210e901f3
Anusha Sharma <as10553@nyu.edu>
2018-04-26 00:16:32 -0400

    fixing reverse match error

--- vital_site/authoring/views/create_view.py
+++ vital_site/authoring/views/create_view.py
@@ -59,7 +59,8 @@ def course_create(request):
         return render(request, 'authoring/course_create.html', {'form': form, 'error_message':error_message})
 
 
-def course_add_vms(request, course_id):
+def course_add_vms(request):
+    course_id = 33
     logger.debug("in course create")
     error_message = ''
     if request.method == 'POST':
