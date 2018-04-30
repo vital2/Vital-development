52adada02602b2faa7dc5c42d085afe3e3632e35
Anusha Sharma <as10553@nyu.edu>
2018-04-29 22:58:36 -0400

    fixing the rediect issue

--- vital_site/authoring/views/create_view.py
+++ vital_site/authoring/views/create_view.py
@@ -55,8 +55,8 @@ def course_create(request):
             course.save()
             logger.debug(type(course.id))
             logger.debug('>>>>>'+'/authoring/courses/'+course.id+'/vms')
-            #return HttpResponseRedirect(reverse('authoring:course_add_vms', kwargs={'course_id': course.id}))
-            return redirect('/authoring/courses/'+course.id+'/vms')
+            return HttpResponseRedirect(reverse('authoring:course_add_vms', kwargs={'course_id': course.id}))
+            #return redirect('/authoring/courses/'+course.id+'/vms')
     else:
         form = CreateCourseForm()
         return render(request, 'authoring/course_create.html', {'form': form, 'error_message': error_message})
