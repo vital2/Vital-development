abd4de7c26cb5fa17c9e6f483d0a73ff32bca793
Anusha Sharma <as10553@nyu.edu>
2018-05-02 22:18:34 -0400

    adding the networking view

--- vital_site/authoring/forms/create_form.py
+++ vital_site/authoring/forms/create_form.py
@@ -21,6 +21,6 @@ class CreateNetworksForm(forms.Form):
     hub_vms = forms.ModelChoiceField(queryset=Virtual_Machine.objects.all())
     vm_iface = forms.CharField(widget=forms.widgets.TextInput)
 
-    def __init__(self, course_id):
-        super(CreateNetworksForm, self).__init__()
+    def __init__(self, course_id, *args, **kwargs):
+        super(CreateNetworksForm, self).__init__(*args, **kwargs)
         self.fields['hub_vms'].queryset = Virtual_Machine.objects.filter(course=course_id)
