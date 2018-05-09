b635673fbde424916fd0f945a24e47bcf4d9a2c3
Anusha Sharma <as10553@nyu.edu>
2018-05-09 16:07:58 -0400

    add more link in addvms

--- vital_site/authoring/views/create_view.py
+++ vital_site/authoring/views/create_view.py
@@ -48,7 +48,8 @@ def course_create(request):
             course = Course()
             course.name = form.cleaned_data['course_name']
             course.course_number = form.cleaned_data['course_number']
-            course.start_date = str(form.cleaned_data['start_date'])
+            print(type(form.cleaned_data['start_date']))
+            course.start_date = form.cleaned_data['start_date']
             course.created_date = datetime.date.today
             user_id = request.user.id
             course.course_owner = user_id
