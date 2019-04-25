e40de6cb8bb6638fb81bf3240990bfcabd17c440
Anoop <as11552@nyu.edu>
2019-04-25 00:18:33 -0400

    Include removal of qcows and conf from datastore.

--- vital_site/vital/management/commands/ResetCourse.py
+++ vital_site/vital/management/commands/ResetCourse.py
@@ -3,9 +3,14 @@ import logging
 from django.utils import timezone
 from vital.models import Course, Registered_Course, User_Network_Configuration, Available_Config, User_VM_Config
 from django.utils.crypto import get_random_string
+import ConfigParser
 
 logger = logging.getLogger(__name__)
+config = ConfigParser.ConfigParser()
+config.optionxform=str
 
+# TODO change to common config file in shared location
+config.read("/home/vital/config.ini")
 
 class Command(BaseCommand):
     help = "resets a course to remove all students and generate a new registration code"
@@ -19,10 +24,29 @@ class Command(BaseCommand):
             required=True
         )
 
+    def delete_student_configs(self, user_id, course_id):
+        logger.debug("Removing User configs...")
+        conf_path = config.get("VMConfig", "VM_CONF_LOCATION")
+        qcow_path = config.get("VMConfig", "VM_QCOW_LOCATION")
+        for vm in course.virtual_machine_set.all():
+            name = user_id + '_' + course_id + '_' + vm.id
+            try:
+                os.remove(conf_path + '/' + name + '.conf')
+                os.remove(qcow_path + '/' + name + '.qcow')
+                logger.debug('Removed files for ' + name)
+            except OSError as e:
+                if e.errno != errno.ENOENT:
+                    logger.error(' Error while removing file - {}'.format(name))
+                    logger.error(str(e).rstrip())
+                    raise Exception('ERROR : cannot remove the vm - conf '
+                                    '\n Reason : %s' % str(e).rstrip())
+
     def handle(self, *args, **options):
         course_id = options['course_id']
         course = Course.objects.get(id=course_id)
         print "Removing course: "+course.name+" (ID:"+ str(course.id) +")"
+        for user_id in (Registered_Course.objects.filter(course=course)).values("user_id", flat=True):
+            self.delete_student_configs(user_id, course_id)
         print "Removing registered students"
         Registered_Course.objects.filter(course=course).delete()
         print "Removing registered user network configs"
