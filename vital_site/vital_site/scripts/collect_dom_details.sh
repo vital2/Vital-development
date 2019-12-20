fb65605481c11848417993ad2707c948996b6f71
reg1reg1 <yuvraj.singh19@gmail.com>
2019-12-20 13:02:41 -0500

    Attempt to fix tree redundancy

--- vital_site/vital_site/scripts/collect_dom_details.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# this is called by systemd on apache2 start - vital_collect_dom_details.service
-# workon vital - instead point to virtualenv python installation
-/home/vital/.virtualenvs/vital/bin/python /home/vital/vital2.0/source/virtual_lab/vital_site/manage.py CollectXenDomDetails
--- vital_site/vital_site/scripts/listen_xen_stats.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# this is called by upstart job on apache2 start - /etc/init/upstart/vital_listen_xen_stats.conf
-# workon vital - instead point to virtualenv python installation
-/home/vital/.virtualenvs/vital/bin/python /home/vital/vital2.0/source/virtual_lab/vital_site/manage.py StartServerStatsCollection
